d765f89ea2e00808bfcd266903665aeba43e9fce
rahulkuanr <47236527+rahulkuanr@users.noreply.github.com>
2022-07-08 23:03:35 +0530

    add a controller and router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,3 @@
+module.exports.home = (request, response) => {
+    return response.end('<h1>Express is ready for Xial!</h1>');
+};
--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const port = 8000;
 
 const app = express();
 
+//using express router to route
+app.use('/', require('./routes'));
+
 app.listen(port, (error) => {
     if(error){
         console.log(`Error in running the server: ${error}`);
