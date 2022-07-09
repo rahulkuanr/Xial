d2b7cb2fcce03619d8bb3703e7200e5415aacd8e
rahulkuanr <47236527+rahulkuanr@users.noreply.github.com>
2022-07-09 18:18:43 +0530

    add form to signup and signin page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -18,3 +18,13 @@ module.exports.signIn = (request, response) => {
         title: 'Xial | Sign In'
     });
 };
+
+// get sign up data
+module.exports.create = (request, response) => {
+    //todo later
+};
+
+//sign in and create session for the user
+module.exports.createSession = (request, response)=> {
+    //todo later
+};
