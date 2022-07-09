a9a55fcb2a040db5f087fb1a4bafc8672930f160
rahulkuanr <47236527+rahulkuanr@users.noreply.github.com>
2022-07-09 18:44:39 +0530

    create user and add to db

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,30 +1,54 @@
+const User = require("../models/user");
+
 // render user profile
 module.exports.profile = (request, response) => {
-    return response.render('user_profile', {
-        title: "User Profile"
-    });
+  return response.render("user_profile", {
+    title: "User Profile",
+  });
 };
 
 // render the signup page
 module.exports.signUp = (request, response) => {
-    return response.render('user_sign_up', {
-        title: 'Xial | Sign Up'
-    });
+  return response.render("user_sign_up", {
+    title: "Xial | Sign Up",
+  });
 };
 
 // render the sign in page
 module.exports.signIn = (request, response) => {
-    return response.render('user_sign_in', {
-        title: 'Xial | Sign In'
-    });
+  return response.render("user_sign_in", {
+    title: "Xial | Sign In",
+  });
 };
 
 // get sign up data
 module.exports.create = (request, response) => {
-    //todo later
+  if (request.body.password != request.body.confirmPassword) {
+    return response.redirect("back");
+  }
+
+  User.findOne({ email: request.body.email }, (error, user) => {
+    if (error) {
+      console.log("Error in finding user while sign up");
+      return;
+    }
+
+    if (!user) {
+      User.create(request.body, (error, user) => {
+        if (error) {
+          console.log("Error in creating user");
+          return;
+        }
+
+        return response.redirect('/users/sign-in');
+      });
+    } else {
+        return response.redirect("back");
+    }
+  });
 };
 
 //sign in and create session for the user
-module.exports.createSession = (request, response)=> {
-    //todo later
+module.exports.createSession = (request, response) => {
+  //todo later
 };
