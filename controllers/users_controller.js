fdd8b4308661b10d6403e87bb3170f5b4383324b
rahulkuanr <47236527+rahulkuanr@users.noreply.github.com>
2022-07-09 17:58:58 +0530

    add signUp and signIn pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,20 @@
+// render user profile
 module.exports.profile = (request, response) => {
     return response.render('user_profile', {
         title: "User Profile"
+    });
+};
+
+// render the signup page
+module.exports.signUp = (request, response) => {
+    return response.render('user_sign_up', {
+        title: 'Xial | Sign Up'
+    });
+};
+
+// render the sign in page
+module.exports.signIn = (request, response) => {
+    return response.render('user_sign_in', {
+        title: 'Xial | Sign In'
     });
 };
