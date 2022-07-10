7776a6f94f88d481a6a5502be3613a545eca75b7
rahulkuanr <47236527+rahulkuanr@users.noreply.github.com>
2022-07-11 04:33:32 +0530

    display posts and related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,19 @@
+const Post = require('../models/post')
+
 module.exports.home = (request, response) => {
-    return response.render('home', {
-        title: "Xial"
+
+    // Post.find({}, (error, posts) => {
+    //     return response.render('home', {
+    //         title: "Xial | Home",
+    //         posts: posts
+    //     });
+    // })
+
+    //populate the user for each post and send it to the view
+    Post.find({}).populate('user').exec((error, posts) => {
+        return response.render('home', {
+            title: "Xial | Home",
+            posts: posts
+        });
     });
 };
